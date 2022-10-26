cb42ca1bda37033d8baf226ce49cdb38685041a6
Mario Granda <mario-granda@hotmail.com>
2022-10-26 16:48:54 +0200

    Add Connect Metamask functionality

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,30 @@
+import { ethers } from "ethers";
+import { useState } from "react";
 import "./style.css";
 
 const NavBar = () => {
+  const [userWallet, setUserWallet] = useState(null);
+
+  const connectWallet = async () => {
+    if (userWallet) {
+      return;
+    }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.send("eth_requestAccounts", []);
+    if (accounts.length === 0) {
+      return;
+    }
+    setUserWallet(accounts[0]);
+  };
   return (
     <header>
       <div className="navbar">
-        <button className="connect-metamask">
-          <p>Connect Metamask</p>
+        <button
+          className="connect-metamask"
+          onClick={connectWallet}
+          disabled={userWallet !== null}
+        >
+          <p>{userWallet ?? "Connect Metamask"}</p>
         </button>
       </div>
     </header>
