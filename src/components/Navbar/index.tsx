ed65ca4485e03da4a5fb15a27bf9b7233b36e1d1
Mario Granda <mario-granda@hotmail.com>
2022-10-26 16:27:11 +0200

    Add Navbar and define style

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const NavBar = () => {
+  return (
+    <header>
+      <div className="navbar">
+        <button className="connect-metamask">
+          <p>Connect Metamask</p>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
