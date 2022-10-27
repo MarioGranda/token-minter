f0d03056b84674102f4d842356e54a25ff2f7f18
Mario Granda <mario-granda@hotmail.com>
2022-10-27 09:52:25 +0200

    Add token data React states

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import "./style.css";
 
 function App() {
+  const [userBalance, setUserBalance] = useState(null);
+  const [token, setToken] = useState({
+    name: "",
+    symbol: "",
+  });
+
   return (
     <div className="App">
       <div className="token-data">
-        <p>Token name:</p>
-        <p>Token symbol:</p>
-        <p>User balance:</p>
+        <p>Token name: {token.name}</p>
+        <p>Token symbol: {token.symbol}</p>
+        <p>User balance: {userBalance}</p>
       </div>
       <div>
         <div className="mint-tokens">
