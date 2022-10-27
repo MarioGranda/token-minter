dd6a0ffe5d360d885e8258990a4ab2a9141b5f50
Mario Granda <mario-granda@hotmail.com>
2022-10-27 09:22:37 +0200

    Add input and button to App Component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -8,6 +8,12 @@ function App() {
         <p>Token symbol:</p>
         <p>User balance:</p>
       </div>
+      <div>
+        <div>
+          <input placeholder="Insert address" value=""></input>
+          <button>Mint Tokens</button>
+        </div>
+      </div>
     </div>
   );
 }
