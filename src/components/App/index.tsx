0379def4fc8eafad1f89a7be7ff6a7562110018c
Mario Granda <mario-granda@hotmail.com>
2022-10-27 09:12:32 +0200

    Add token info elements to App Component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,15 @@
 import "./style.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <p>Token name:</p>
+        <p>Token symbol:</p>
+        <p>User balance:</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
