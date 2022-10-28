0e4bf289187ee38e9cda638f20be025fb2f410eb
Mario Granda <mario-granda@hotmail.com>
2022-10-28 10:44:17 +0200

    Add textarea change test

---
+++ src/components/App/app.test.tsx
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from ".";
+
+jest.mock("../../utils/tokenContract/getFunctions", () => ({
+  getTokenContract() {
+    return "mock contract";
+  },
+  getTokenName() {
+    return "mock name";
+  },
+  getTokenSymbol() {
+    return "mock symbol";
+  },
+  getUserBalance() {
+    return "mock balance";
+  },
+}));
+
+test("textarea", () => {
+  render(<App />);
+  const textarea = screen.getByPlaceholderText("Insert address");
+  fireEvent.change(textarea, {
+    target: { value: "0x1DB4AFfFa9235478Ff5d5ec90F521E65ec2dF685" },
+  });
+  expect(
+    screen.getByDisplayValue("0x1DB4AFfFa9235478Ff5d5ec90F521E65ec2dF685")
+  ).toBeInTheDocument();
+});
