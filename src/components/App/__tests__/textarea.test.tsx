14579ec5ef43c7fcfe2915913d41fde16c6e9d53
Mario Granda <mario-granda@hotmail.com>
2022-10-28 11:01:49 +0200

    Update imports

--- src/components/App/__tests__/textarea.test.tsx
+++ src/components/App/__tests__/textarea.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import App from ".";
+import App from "..";
 
 jest.mock("../../utils/tokenContract/getFunctions", () => ({
   getTokenContract() {
