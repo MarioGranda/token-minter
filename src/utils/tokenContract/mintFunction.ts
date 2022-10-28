c8a8ab276173aa369a2335ec1c06042ba4f74a51
Mario Granda <mario-granda@hotmail.com>
2022-10-28 10:59:34 +0200

    Add mintTokens function

---
+++ src/utils/tokenContract/mintFunction.ts
@@ -0,0 +1,16 @@
+import { ethers } from "ethers";
+import { abi } from "../../constants/token/abi";
+import { address } from "../../constants/token/address";
+
+const getTokenContract = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = await provider.getSigner();
+  return new ethers.Contract(address, abi, signer);
+};
+
+const mintTokens = async (to: string) => {
+  const contract = await getTokenContract();
+  const tx = await contract.mint(to, 1);
+  const receipt = await tx.wait();
+  return receipt.result;
+};
