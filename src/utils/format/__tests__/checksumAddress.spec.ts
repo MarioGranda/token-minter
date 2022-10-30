b3c17c9eb56dae0f03a7b9c33f6722c20bc55e96
Mario Granda <mario-granda@hotmail.com>
2022-10-30 22:32:38 +0100

    Update checksumAddress util

--- src/utils/format/__tests__/checksumAddress.spec.ts
+++ src/utils/format/__tests__/checksumAddress.spec.ts
@@ -1,11 +1,17 @@
 import { getChecksumAddress } from "../checksumAddress";
 
 describe("checksumAddress", () => {
-  it("should return a fixed length string", () => {
+  it("should return the checksum address", () => {
     const ethAddress = "0xf3a82eb87f58a122ba2ff4e56dbf355361f215a4";
     const checksumAddress = getChecksumAddress(ethAddress);
     expect(checksumAddress).toEqual(
       "0xf3a82EB87F58a122bA2Ff4e56dBf355361f215a4"
     );
+  });
+
+  it("should return an empty string", () => {
+    const ethAddress = null;
+    const checksumAddress = getChecksumAddress(ethAddress);
+    expect(checksumAddress).toEqual("");
   });
 });
--- src/utils/format/checksumAddress.ts
+++ src/utils/format/checksumAddress.ts
@@ -1,7 +1,10 @@
 import { ethers } from "ethers";
 import { addressBytesLength } from "../../constants/address/bytesLength";
 
-export const getChecksumAddress = (address: string) => {
+export const getChecksumAddress = (address: string | null) => {
+  if (!address) {
+    return "";
+  }
   return ethers.utils.getAddress(
     ethers.utils.hexZeroPad(
       ethers.utils.hexStripZeros(address),
